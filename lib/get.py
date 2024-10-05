68f0923fa45662d38ce712000d98c9032e64fa43
berryrexs <nathan.mwai@student.moringaschool.com>
2024-10-05 07:39:35 +0300

    fills in what I learnt

--- lib/get.py
+++ lib/get.py
@@ -4,7 +4,8 @@ import json
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
 
 response = requests.get(url)
-
-json_content = json.loads(response.text)
-
-print(json.dumps(json_content, indent=4))
+print(response.text) # This one shows the html 
+print(response.content) # Displays in json format
+json_content = json.loads(response.text) 
+# json.loads(response.content) This one is less organised but still looks organized
+print(json.dumps(json_content, indent=4)) #Displays in a more organised format
